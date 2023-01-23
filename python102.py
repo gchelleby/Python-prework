10950851b5e5a8eb9ed9fbd24b68e29b67004bb2
Michelle Green <gchelleby@gmail.com>
2023-01-22 23:12:54 -0600

    first commit

---
+++ python102.py
@@ -0,0 +1,51 @@
+#Question 1
+#Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
+
+def hello_name(user_name):
+    """Print a hello message to users."""
+    for name in user_name:
+        msg = "Hello, " + user_name.upper() + "!"
+        print(msg)
+    hello_name()
+
+#Question 2
+#Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
+
+def first_odds():
+    current_number = 0
+    while current_number < 100:
+        current_number += 1
+        if current_number % 2 == 0:
+            continue
+    print(current_number)
+
+#Question 3
+#Please write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.
+
+def max_num_in_list(a_list):
+    digits = [5, 8, 47, 84, 251, 2]
+    max(digits)
+
+#I'm sorry, I didn't know if I was supposed to put in numbers myself or if there was a different solution, but this is the one I found. I hope thats okay!
+
+#Question 4
+#Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
+
+def is_leap_year(a_year):
+    year = input("\nPlease enter the year you want to test.")
+    year = int(year)
+    if year % 4 == 0:
+        print("Your year is a leap year!")
+    if year % 400 == 0:
+        print("Your year is a leap year!")
+    else:
+        print("Your year is not a leap year.")
+
+#Question 5
+#Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
+
+def is_consecutive(a_list):
+    return sorted() == list(range(min(3)+1))
+test = [3, 6, 5, 4, 7]
+test2 = [3, 10, 5, 4, 7]
+print(is_consecutive())
